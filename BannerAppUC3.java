54b8da9c86a176fe47b633f55c526b4235b97ee8
Sravya3937 <pp3049@srmist.edu.in>
2026-02-23 12:16:39 +0530

    UC3 OOPS Banner App

---
+++ BannerAppUC3.java
@@ -0,0 +1,31 @@
+public class BannerAppUC3 {
+
+    public static void main(String[] args) {
+
+        // Each line of OOPS banner using String.join()
+
+        String line1 = String.join(" ",
+                " ***** ", " ***** ", " ******", " ***** ");
+        String line2 = String.join(" ",
+                "*     *", "*     *", "*     *", "*     *");
+        String line3 = String.join(" ",
+                "*     *", "*     *", "*     *", "*      ");
+        String line4 = String.join(" ",
+                "*     *", "*     *", "****** ", " ***** ");
+        String line5 = String.join(" ",
+                "*     *", "*     *", "*      ", "      *");
+        String line6 = String.join(" ",
+                "*     *", "*     *", "*      ", "*     *");
+        String line7 = String.join(" ",
+                " ***** ", " ***** ", "*      ", " ***** ");
+
+        // Print banner
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
